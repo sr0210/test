5fd91bf1b073d2140fae8bda44a19ada9b196f0c
shirui <shirui_0210@163.com>
2017-06-25 14:36:17 +0800

    create project

---
+++ test-sr/src/main/java/com/sr/shirui/App.java
@@ -0,0 +1,13 @@
+package com.sr.shirui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
