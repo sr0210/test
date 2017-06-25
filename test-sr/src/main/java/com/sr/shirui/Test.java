cace494eb90ec8f6bdea1550c26e824b63043a46
shirui <shirui_0210@163.com>
2017-06-25 14:45:13 +0800

    update

---
+++ test-sr/src/main/java/com/sr/shirui/Test.java
@@ -0,0 +1,12 @@
+package main.java.com.sr.shirui;
+
+/**
+ * Created by shirui on 2017/6/25.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("aaa");
+    }
+
+}
